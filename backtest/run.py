db1a19d161ba28ae94a407f258591a33c96ff6ed
Samuel <samuelperron@hotmail.fr>
2020-12-06 20:04:18 -0500

    T-8500 - Use right launcher

--- backtest/run.py
+++ backtest/run.py
@@ -5,6 +5,6 @@ from strategies.MA import strategy
 # To test a specific strategy, create the class with the strategy prop and pass the strategy id.
 # To test everything, create the class without any props.
 
-# Launcher(strategy=3, intraday=True, symbol='AMD')
+Launcher(strategy=strategy, intraday=False)
 
-Backtest('SHOP.TO', strategy)
+# Backtest('SHOP.TO', strategy)
