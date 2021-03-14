d0774c041a8653ae2b034bb1439296c4ea114084
Samuel <samuelperron@hotmail.fr>
2021-03-14 19:52:29 -0400

    T-8560 - Methods for Account

--- alpaca_paper/llama/account/models.py
+++ alpaca_paper/llama/account/models.py
@@ -1,5 +1,7 @@
 from .. import db
 from ..base import BaseDBModel
+from ..base.utils import clock
+from datetime import datetime, timedelta
 
 
 association_table_account_historical_equities = db.Table(
@@ -33,19 +35,42 @@ class Account(db.Model, BaseDBModel):
         """
         Total value of cash + holding positions
         """
-        pass
+        equity = self.cash + sum((p.market_value() for p in self.positions))
+
+        historical = HistoricalEquity(
+            timestamp=datetime.now(),
+            equity=equity
+        )
+        historical.save_to_db()
+        self.historical_equities.append(historical)
+        self.save_to_db()
+
+        return equity
 
     def last_equity(self):
         """
         Equity as of previous trading day at 16:00:00 ET
         """
-        pass
+        grace_period = 5
+
+        last_close = datetime.strptime(clock()['last_close'], '%Y-%m-%dT%H:%M')
+        start_grace = last_close - timedelta(minutes=grace_period)
+        finish_grace = last_close + timedelta(minutes=grace_period)
+
+        for h in self.historical_equities:
+            if h.timestamp >= start_grace and h.timestamp <= finish_grace:
+                return h.equity
+        
+        return None
 
     def buying_power(self):
         """
         Current available $ buying power
         """
-        pass
+        # Since orders are filled instantly for now, buying power
+        # will always equal cash on hand. 
+        # TODO: Substract cash locked up in open orders
+        return self.cash
 
     def json(self):
         return {
--- alpaca_paper/llama/base/utils.py
+++ alpaca_paper/llama/base/utils.py
@@ -1,4 +1,6 @@
 from .. import ACCOUNT_ID, ALPACA_UID, ALPACA_SECRET, ACOUNT_BASE_CASH
+import requests
+from datetime import datetime, timedelta
 
 
 def get_current_account():
@@ -39,3 +41,24 @@ def bars(symbols, timeframe, limit=200):
 
     response = alpaca('get', f'bars/{timeframe}', params=params)
     return response.json()
+
+
+def get_last_close():
+    date_format = '%Y-%m-%d'
+
+    today = datetime.now().date()
+    end_date = (today - timedelta(days=1)).strftime(date_format)
+    start_date = (today - timedelta(days=10)).strftime(date_format)
+    
+    last_market_day = alpaca('get', 'calendar', params={
+        'start': start_date, 
+        'end': end_date
+    }).json()[-1]
+
+    return f"{last_market_day['date']}T{last_market_day['close']}"
+
+
+def clock():
+    data = alpaca('get', 'clock').json()
+    data['last_close'] = get_last_close()
+    return data
